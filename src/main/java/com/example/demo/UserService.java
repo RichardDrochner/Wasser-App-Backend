1fd7e7f3fe1adfbbf80e77d1ec2cf0afbeb27d4e
RichardDrochner <len_drochner@web.de>
2023-05-02 13:26:05 +0200

    UserService und Repository changes

--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -1,5 +1,29 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-    
+    @Autowired
+    UserRepository repo;
+
+    public User save(User user) {
+        return repo.save(user);
+    }
+
+    public User get(Long id) {
+        return repo.findById(id).orElseThrow(() -> new RuntimeException());
+    }
+
+    public List<User> getAll() {
+        Iterable<User> iterator = repo.findAll();
+        List<User> users = new ArrayList<User>();
+        for (User user : iterator)
+            users.add(user);
+        return users;
+    }
+
 }
