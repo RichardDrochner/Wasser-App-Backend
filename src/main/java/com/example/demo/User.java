58e8b9185a64ec2eee4ac77c5c72a25c37e8aa53
chzn03 <alimzokam@gmail.com>
2023-05-02 13:34:00 +0200

    User Klasse (überschriebene Methoden fehlen !!)

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,5 +1,60 @@
 package com.example.demo;
 
-public class User {
-    
-}
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.sql.Time;
+import java.util.Date;
+
+
+    @Entity
+    public class User{
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+        private Long id;
+        private String name;
+        private int amount;
+        private Time time;      // möglicherweise Datentyp ändern
+
+
+        public User() {}
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public void setAmount(int amount) {
+            this.amount = amount;
+        }
+
+        public Time getTime() {
+            return time;
+        }
+
+        public void setTime(Time time) {
+            this.time = time;
+        }
+
+
+    }
+
+
