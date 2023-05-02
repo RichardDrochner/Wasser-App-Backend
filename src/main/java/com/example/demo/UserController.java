e7da87e508a4a8cefa142406b3e149b3019dbc36
RichardDrochner <len_drochner@web.de>
2023-05-02 13:33:31 +0200

    User Controller nicht fertig

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,31 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
-    
+
+    @Autowired
+    UserService service;
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @PostMapping("/User")
+    public User createUser(@RequestBody User user) {
+        return service.save(user);
+    }
+
+    @GetMapping("/things/{id}")
+    public User getUser(@PathVariable String id) {
+        logger.info("GET request on route User with {}", id);
+        Long UserId = Long.parseLong(id);
+        return service.get(UserId);
+    }
 }
